e569f6377f3a5be1a79fb19cd264e41d9a5d2683
Rashi14111 <rashiigarg1411@gmail.com>
2025-08-04 14:33:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -664,8 +664,12 @@ def submit_capacity():
     except Exception as e:
         return f"An error occurred: {e}", 500
 
-if __name__ == '__main__':
-    app.run()
+port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+
+    
+    
+
+
+ 
